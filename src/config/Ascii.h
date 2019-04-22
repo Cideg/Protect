25ec619fafdde091696bae3596461ad1a6e91f5f
Cideg <Cideg@web.de>
2019-04-22 19:38:28 +0200

    first commit

---
+++ src/config/Ascii.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2018, The TurtleCoin Developers
+// Copyright (c) 2019, The Protect Project
+//
+// Please see the included LICENSE file for more information
+
+#pragma once
+
+const std::string asciiArt =
+"\n PPPPPPPPPPPPPPPPP                                                 tttt                                                           tttt  \n"             
+"   P::::::::::::::::P                                             ttt:::t                                                        ttt:::t  \n"             
+"   P::::::PPPPPP:::::P                                            t:::::t                                                        t:::::t   \n"            
+"   PP:::::P     P:::::P                                           t:::::t                                                        t:::::t     \n"          
+"    P::::P     P:::::Prrrrr   rrrrrrrrr      ooooooooooo   ttttttt:::::ttttttt        eeeeeeeeeeee        ccccccccccccccccttttttt:::::ttttttt  \n"       
+"    P::::P     P:::::Pr::::rrr:::::::::r   oo:::::::::::oo t:::::::::::::::::t      ee::::::::::::ee    cc:::::::::::::::ct:::::::::::::::::t   \n"      
+"    P::::PPPPPP:::::P r:::::::::::::::::r o:::::::::::::::ot:::::::::::::::::t     e::::::eeeee:::::ee c:::::::::::::::::ct:::::::::::::::::t   \n"      
+"    P:::::::::::::PP  rr::::::rrrrr::::::ro:::::ooooo:::::otttttt:::::::tttttt    e::::::e     e:::::ec:::::::cccccc:::::ctttttt:::::::tttttt    \n"     
+"    P::::PPPPPPPPP     r:::::r     r:::::ro::::o     o::::o      t:::::t          e:::::::eeeee::::::ec::::::c     ccccccc      t:::::t          \n"     
+"    P::::P             r:::::r     rrrrrrro::::o     o::::o      t:::::t          e:::::::::::::::::e c:::::c                   t:::::t          \n"     
+"    P::::P             r:::::r            o::::o     o::::o      t:::::t          e::::::eeeeeeeeeee  c:::::c                   t:::::t           \n"    
+"    P::::P             r:::::r            o::::o     o::::o      t:::::t    tttttte:::::::e           c::::::c     ccccccc      t:::::t    tttttt  \n"   
+"  PP::::::PP           r:::::r            o:::::ooooo:::::o      t::::::tttt:::::te::::::::e          c:::::::cccccc:::::c      t::::::tttt:::::t  \n"   
+"  P::::::::P           r:::::r            o:::::::::::::::o      tt::::::::::::::t e::::::::eeeeeeee   c:::::::::::::::::c      tt::::::::::::::t   \n"  
+"  P::::::::P           r:::::r             oo:::::::::::oo         tt:::::::::::tt  ee:::::::::::::e    cc:::::::::::::::c        tt:::::::::::tt  \n"   
+"  PPPPPPPPPP           rrrrrrr               ooooooooooo             ttttttttttt      eeeeeeeeeeeeee      cccccccccccccccc          ttttttttttt   \n";
+
